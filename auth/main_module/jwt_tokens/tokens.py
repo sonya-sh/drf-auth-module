b1b4483a142f411e030404c83bfd35ba48b1f73a
sonya-sh <sonya.shh@mail.ru>
2024-04-07 23:31:42 +0300

    Удаление истекших токенов из blacklist

--- auth/main_module/jwt_tokens/tokens.py
+++ auth/main_module/jwt_tokens/tokens.py
@@ -8,10 +8,12 @@ from rest_framework_simplejwt.tokens import AccessToken, Token, BlacklistMixin
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.serializers import AuthUser
 from rest_framework_simplejwt.settings import api_settings
-
+from rest_framework_simplejwt.utils import datetime_from_epoch
 
 redis_client = redis.StrictRedis(host=settings.REDIS_HOST,
                                   port=settings.REDIS_PORT, db=0)
+
+from datetime import datetime, timedelta
 
 
 class CustomBlacklistMixin(BlacklistMixin):
@@ -24,26 +26,28 @@ class CustomBlacklistMixin(BlacklistMixin):
                 raise TokenError(_("Token is blacklisted"))
 
         def blacklist(self) -> None:
+
             jti = self.payload[api_settings.JTI_CLAIM]
             exp = self.payload["exp"]
         
             token_str = str(self)
-            redis_client.setex(jti, exp, token_str)
+            exp_seconds = (datetime_from_epoch(exp) - timezone.now()).total_seconds()  # Вычисляем время жизни токена в секундах
+            redis_client.setex(jti, int(exp_seconds), token_str)
 
-        @classmethod
-        def for_user(cls, user: AuthUser) -> Token:
+        # @classmethod # м б вызван без создания экземпляра класса
+        # def for_user(cls, user: AuthUser) -> Token:
 
-            token = super().for_user(user)  # type: ignore
+        #     token = super().for_user(user)  # type: ignore
 
-            jti = token["jti"]
-            exp = token["exp"]
+        #     jti = token["jti"]
+        #     exp = token["exp"]
 
-            redis_client.setex(jti, exp - timezone.now, str(token))
+        #     #exp_seconds = (datetime_from_epoch(exp) - timezone.now()).total_seconds()
+        #     #redis_client.setex(jti, int(exp_seconds), str(token))
+        #     return token
 
 
-            return token
-
-
+        
 class AccessToken(Token):
     token_type = "access"
     lifetime = api_settings.ACCESS_TOKEN_LIFETIME
